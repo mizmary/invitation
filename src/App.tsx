dd178f82be23b6eada64530c4ff713bce349d4be
miz.mary <simk.otar@gmail.com>
2025-06-01 23:13:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import main from './assets/main.png'
+import textureFirst from './assets/second.svg'
+import bg from './assets/bg.svg'
+import background from './assets/background.svg'
+
+
+function App() {
+
+  return (
+  <div className='container'>
+    <img src={background} className='bg'/>
+  </div>
+  )
+}
+
+export default App
