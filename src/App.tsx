45a287ec89f78c6685b6a12f429af8c7e4f2eaea
miz.mary <simk.otar@gmail.com>
2025-06-02 17:41:00 +0300

    fonts, intro, calendar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,26 @@ function App() {
 
   return (
   <div className='container'>
-    <img src="/testBg.png" className='bg'/>
+    <img src="/images/background.png" className='bg'/>
+    <div className = 'mainTitle-wrap'>
+      <span className='title'>
+        Артем
+      </span>
+      <span className='title' style={{marginLeft: '130px'}}>
+        и
+      </span>
+      <span className='title' style={{marginLeft: '30px'}}>
+        Александра
+      </span>
+    </div>
+    <div className='intro'>
+      <span className='title secondary'>Наши дорогие и любимые</span>
+      <p className='text intro-text'>
+        С радостью спешим Вам сообщить, что не так давно мы приняли одно очень важное для нас решение: стать семьёй!
+Если Вы читаете эту радостную новость, то это значит, что мы хотим разделить этот момент вместе с Вами.
+      </p>
+    </div>
+    <div className='calendar'></div>
   </div>
   )
 }
